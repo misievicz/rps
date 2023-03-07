4f510379d97e93f067ef59901e2c133e4ca31bd6
misievicz <misievicz@gmail.com>
2023-03-07 07:38:22 +0100

    add most functionalities

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
+const myArray = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) return "rock";
-    else if (randomNumber === 1) return "paper";
-    else return "scissors";
+  return myArray[~~(Math.random() * myArray.length)];
 }
 
-let computerChoice = getComputerChoice();
-
-function playRound(computerChoice, playerChoice) {
-  if (playerChoice === computerChoice) return 2;
-  else if (playerChoice === "rock" && computerChoice === "paper") return 0;
-  else if (playerChoice === "rock" && computerChoice === "scissors") return 1;
-  else if (playerChoice === "paper" && computerChoice === "rock") return 1;
-  else if (playerChoice === "paper" && computerChoice === "scissors") return 0;
-  else if (playerChoice === "scissors" && computerChoice === "rock") return 0;
-  else if (playerChoice === "scissors" && computerChoice === "paper") return 1;
-}
-
-function game() {
-    for (let i = 1; i < 6; i++) {
-        let playerChoice = prompt("Please input your choice").toLowerCase();
-        let result = playRound(computerChoice, playerChoice);
-        if (result === 1) playerScore++;
-        else if (result === 0) computerScore++;
-        console.log(playerScore, computerScore, i);
-    }
-    if (playerScore > computerScore) console.log("You win!!!");
-    else if (playerScore < computerScore) console.log("You lose!!!");
-    else console.log("Draw!!!");
-}
 
 let playerScore = 0;
 let computerScore = 0;
-game();
+
+function getPlayerChoice(item) {
+  return item;
+} 
+
+function disableButtons() {
+  buttons.forEach((item) => {
+    item.disabled = true;
+  });
+}
+
+function playRound(playerChoice) {
+  let computerChoice = getComputerChoice();
+  if (playerChoice === computerChoice) return 0;
+  else if (playerChoice === "rock" && computerChoice === "paper") return computerScore++;
+  else if (playerChoice === "rock" && computerChoice === "scissors") return playerScore++;
+  else if (playerChoice === "paper" && computerChoice === "rock") return playerScore++;
+  else if (playerChoice === "paper" && computerChoice === "scissors") return computerScore++;
+  else if (playerChoice === "scissors" && computerChoice === "rock") return computerScore++;
+  else if (playerChoice === "scissors" && computerChoice === "paper") return playerScore++;
+}
+
+const buttons = document.querySelectorAll('input')
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        playRound(button.value); 
+        score.textContent = "You win this round!";
+        console.log(playerScore);
+        console.log(computerScore);
+
+        if (playerScore === 5 || computerScore === 5) {
+            console.log("end");
+            disableButtons();
+        }
+    })
+});
+
+const score = document.querySelector(".score");
+
+
+ 
+
+
+
 
 
