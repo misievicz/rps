6d041f5fbcb40cfd5729ccc66a29a354459288f6
misievicz <misievicz@gmail.com>
2023-03-07 08:16:48 +0100

    finish version REVISITING RPS

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function getComputerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
-function getPlayerChoice(item) {
-  return item;
-} 
+// function getPlayerChoice(item) {
+//   return item;
+// } 
 
 function disableButtons() {
   buttons.forEach((item) => {
@@ -19,33 +19,47 @@ function disableButtons() {
 }
 
 function playRound(playerChoice) {
-  let computerChoice = getComputerChoice();
-  if (playerChoice === computerChoice) return 0;
-  else if (playerChoice === "rock" && computerChoice === "paper") return computerScore++;
-  else if (playerChoice === "rock" && computerChoice === "scissors") return playerScore++;
-  else if (playerChoice === "paper" && computerChoice === "rock") return playerScore++;
-  else if (playerChoice === "paper" && computerChoice === "scissors") return computerScore++;
-  else if (playerChoice === "scissors" && computerChoice === "rock") return computerScore++;
-  else if (playerChoice === "scissors" && computerChoice === "paper") return playerScore++;
+    let computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice) score.textContent = "Draw!";
+    else if ((playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors") ||
+        (playerChoice === "scissors" && computerChoice === "rock")) {
+        score.textContent = "Computer wins this round!";
+        computerScore++;
+        compScore.textContent = `Computer: ${computerScore}`
+    }
+
+    else if ((playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")) {
+        score.textContent = "You win this round!";
+        playerScore++;
+        playScore.textContent = `Player: ${playerScore}`;
+    }
 }
 
 const buttons = document.querySelectorAll('input')
 buttons.forEach(button => {
     button.addEventListener('click', function () {
-        playRound(button.value); 
-        score.textContent = "You win this round!";
+        playRound(button.value);
         console.log(playerScore);
         console.log(computerScore);
-
-        if (playerScore === 5 || computerScore === 5) {
-            console.log("end");
+        if (playerScore === 5) {
+            title.textContent = "game over."
+            score.textContent = "YOU WIN THE GAME.";
+            disableButtons();
+        } else if (computerScore === 5) {
+            title.textContent = "game over.";
+            score.textContent = "COMPUTER WINS THE GAME.";
             disableButtons();
         }
     })
 });
 
 const score = document.querySelector(".score");
-
+const title = document.querySelector(".title");
+const compScore = document.querySelector(".comp-score");
+const playScore = document.querySelector(".play-score");
 
  
 
